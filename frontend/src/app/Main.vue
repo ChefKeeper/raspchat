d7ac71ea94ebedabedfe240fc0877068229bd0a0
Zohaib <zohaib@creapptives.com>
2017-03-12 22:01:02 -0700

    Creating structure for vue2 projects to deprecate hardcoded statics

---
+++ frontend/src/app/Main.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+split-pane.root
+  p(slot="left") Hello
+  p(slot="right") World
+</template>
+
+<script>
+import SplitPane from './containers/SplitPane.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    'split-pane': SplitPane
+  }
+}
+</script>
---
+++ frontend/src/app/containers/SplitPane.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.split-pane
+  .columns
+    .column-left
+      slot(name="left")
+    .column-right
+      slot(name="right")
+</template>
+
+<script>
+export default {
+  name: 'SplitPane',
+  data() {
+    return {}
+  }
+}
+</script>
