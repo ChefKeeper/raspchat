f07480b2744833d1d0b5aca6f6113078b61cd872
Zohaib <zohaib@creapptives.com>
2017-03-18 12:21:40 -0700

    Using vue-cli for project structure instead of stupid yo!

---
+++ frontend/src/components/SplitPane.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+.split-pane
+  .columns
+    .column-left(v-bind:style="{width: leftWidth, display: leftDisplay}")
+      .column-container
+        slot(name="left")
+    .column-right(v-bind:style="{width: rightWidth, display: rightDisplay}")
+      .column-container
+        slot(name="right")
+</template>
+
+
+<style lang="scss">
+@import '../assets/styles/split-pane';
+@include panel-skin($foreground1, $background1);
+</style>
+
+<script>
+export default {
+  name: 'SplitPane',
+  props: {
+    ratio: {
+      type: Number,
+      default: 0.5,
+      validator: (v) => v >= 0 && v <= 1
+    },
+
+    hideLimit: {
+      type: Number,
+      default: 0.1,
+      validator: (v) => v >= 0 && v <= 1
+    }
+  },
+
+  computed: {
+    leftWidth() {
+      return Math.floor(this.ratio * 100) + '%'
+    },
+
+    rightWidth() {
+      return Math.floor((1 - this.ratio) * 100) + '%'
+    },
+
+    leftDisplay() {
+      return this.ratio < this.hideLimit ? 'none' : 'block';
+    },
+
+    rightDisplay() {
+      return 1 - this.ratio < this.hideLimit ? 'none' : 'block';
+    }
+  },
+
+  data() {
+    return {}
+  }
+}
+</script>
